d449505a5a05e090eb090253bcc1afbdc701f370
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-04-19 09:36:28 +0700

    feat: init chapter part-2

--- Chapter 1/Landmark list/Landmark list/Views/ContentView.swift
+++ Chapter 1/Landmark list/Landmark list/Views/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Land mark list app")
-        }
-        .padding()
+        LandmarkList()
     }
 }
 
