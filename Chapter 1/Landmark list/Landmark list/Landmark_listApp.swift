29f871d8b960c4f35ca57168d7dd4eeb9da2f40e
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-04-19 08:05:35 +0700

    feat: init Chapter 1 part 2

---
+++ Chapter 1/Landmark list/Landmark list/Landmark_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_listApp.swift
+//  Landmark list
+//
+//  Created by Alizaenal Abidin on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
