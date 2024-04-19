6ecb8771a2f72bc0298e5283ca0ef19d5fcac839
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-04-19 07:55:23 +0700

    feat: section 6

--- Chapter 1/Creating and combining views .swiftpm/CircleImage.swift
+++ Chapter 1/Creating and combining views .swiftpm/CircleImage.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("turtlerock@2x")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 350)
             .clipShape(.circle)
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
