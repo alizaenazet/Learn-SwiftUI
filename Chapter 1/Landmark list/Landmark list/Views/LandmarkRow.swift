e38f98542a4dad362763f09d39a27590f53acb16
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-04-19 08:27:24 +0700

    feat: section 1

---
+++ Chapter 1/Landmark list/Landmark list/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmark list
+//
+//  Created by Alizaenal Abidin on 19/04/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
--- Chapter 1/Landmark list/Landmark list/Views/Landmark_listApp.swift
+++ Chapter 1/Landmark list/Landmark list/Views/Landmark_listApp.swift
@@ -14,4 +14,5 @@ struct Landmark_listApp: App {
             ContentView()
         }
     }
+    
 }
