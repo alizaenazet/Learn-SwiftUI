6b63f6b9337953620b70bb769b2b1ace10da486a
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-04-25 12:19:28 +0700

    finish: Chapter 1 part 3 section 6

---
+++ Chapter 1/Landmark list/Landmark list/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  Landmark list
+//
+//  Created by Alizaenal Abidin on 25/04/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
